1b8a7c34a6dc6d3867de69e79a39e2475d4a9f58
CarlosSanchezGutierrez <A01412419@tec.mx>
2026-05-12 13:59:59 -0600

    feat persistence: add brigades foreign keys

--- services/api-dotnet/tests/Caritas.Brigadas.Api.Tests/Persistence/EfDeleteBehaviorContractTests.cs
+++ services/api-dotnet/tests/Caritas.Brigadas.Api.Tests/Persistence/EfDeleteBehaviorContractTests.cs
@@ -98,6 +98,13 @@ public sealed class EfDeleteBehaviorContractTests
 
         var expected = new[]
         {
+            "brigades.brigade_services(BrigadeId) -> brigades.brigades(Id) DeleteBehavior=NoAction",
+            "brigades.brigade_services(ServiceId) -> core.services(Id) DeleteBehavior=NoAction",
+            "brigades.brigades(CommunityId) -> brigades.communities(Id) DeleteBehavior=NoAction",
+            "brigades.brigades(MobileUnitId) -> brigades.mobile_units(Id) DeleteBehavior=NoAction",
+            "brigades.brigades(OrganizationId) -> core.organizations(Id) DeleteBehavior=NoAction",
+            "brigades.communities(OrganizationId) -> core.organizations(Id) DeleteBehavior=NoAction",
+            "brigades.mobile_units(OrganizationId) -> core.organizations(Id) DeleteBehavior=NoAction",
             "core.role_permissions(PermissionId) -> core.permissions(Id) DeleteBehavior=NoAction",
             "core.role_permissions(RoleId) -> core.roles(Id) DeleteBehavior=NoAction",
             "core.roles(OrganizationId) -> core.organizations(Id) DeleteBehavior=NoAction",
